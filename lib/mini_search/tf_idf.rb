00200d46f956732606955a9f7803c3b06c56195e
Andrew S Aguiar <andrewaguiar6@gmail.com>
2019-09-26 16:53:35 -0300

    removes unused and wrong tfidf class

--- lib/mini_search/tf_idf.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module MiniSearch
-  class TfIdf
-    def self.calculate(number_of_documents_with_term, number_of_documents)
-      1.0 - (number_of_documents_with_term.to_f / number_of_documents.to_f)
-    end
-  end
-end
